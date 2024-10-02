ed98ec261a787e07e699651e888358f6e57a1196
Shahrukh3009 <shaikhshahrukh12345@gmail.com>
2024-10-02 19:29:38 +0000

    New one

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+alert (3 + 3) //we are using nodejs, not browser
+
+
+let name = "Shahrukh"      // string dataType
+let age = 24               // number dataType
+let isLoggedIn = false     // boolean dataType
+let state;                 // undefined dataType
+let number = null          // null dataType
+
+
+console.log(typeof undefined); //undefined dataType
+console.log(typeof null);      // object dataType
+
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "shahrukh@tcs.com"
+var accountPassword = "12345"
+
+
+// prefer not to use var because of issue in block scope and functional scope
+
+console.table([accountId, accountEmail, accountPassword]);
