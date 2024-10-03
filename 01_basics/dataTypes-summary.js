373a3049fc529992cea8cb7413415f67d19f1d50
Shahrukh3009 <shaikhshahrukh12345@gmail.com>
2024-10-03 17:58:01 +0000

    DataType sumary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,23 @@
+// Primitive dataTypes
+
+// 7 Types : String, Boolean, Number, Symbol, BigInt, Null, Undefined
+
+// Reference (Non-Primitive DataTypes)
+
+// Array, Objects, Functions
+
+//Array 
+
+const heros = ["Shahrukh", "Salman", "Amir"];
+
+//Object
+let myObj = {
+    name:'Shahrukh',
+    id:'123',
+    age:'35'
+}
+
+//Function
+const myFunction = function(){
+    console.log("Hello world")
+}
