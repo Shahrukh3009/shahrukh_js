b70eec1b69e9befad13b40d2b7255f3e9ee0bec4
Shahrukh3009 <shaikhshahrukh12345@gmail.com>
2024-10-03 08:59:47 +0000

    Conversion of dataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "34"
+
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+
+// "34" => "34"
+// "34abc" => NaN
+// true => 1; false=> 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 0=> false
+// 1=> true
+// ""=> false
+// "Shahrukh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
