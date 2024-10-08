ddf65937761fc7ba6ad2340e8426766679716b06
Shahrukh3009 <shaikhshahrukh12345@gmail.com>
2024-10-08 14:35:28 +0000

    Dates in JS

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,13 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toDateString());
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+
+
+let myCreatedDate = new Date(2024, 0, 24);
+console.log(myCreatedDate.toDateString());
+
+
